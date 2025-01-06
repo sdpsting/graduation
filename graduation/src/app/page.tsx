6080e23696c54b4d0aa0607092a7bf510cedf052
sdpsting <aykut.keskin@st.uskudar.edu.tr>
2025-01-06 19:30:26 +0300

    Initial commit

---
+++ graduation/src/app/page.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Home() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">testing 1 2 1 2</h1>
+    </div>
+  );
+}
