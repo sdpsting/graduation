05704ecedd394dcf026a647743955ad79c3f2c55
Bedirhan Talha Ayyıldız <btalhaayy@gmail.com>
2025-01-06 20:31:04 +0300

    deneme

--- graduation/src/app/page.tsx
+++ graduation/src/app/page.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
   return (
     <div className="container mt-5">
-      <h1 className="text-center">testing 1 2 1 2</h1>
+      <h1 className="text-center">testing 1 13213122 1 2</h1>
     </div>
   );
 }
