2a9321b7d8d38a7899df0d20e3b88ac529625eef
sdpsting <aykut.keskin@st.uskudar.edu.tr>
2025-01-14 17:23:35 +0300

    filter

--- graduation/src/app/sell.tsx
+++ graduation/src/app/sell.tsx
@@ -5,28 +5,11 @@ import '../app/globals.css';
 import Link from 'next/link';
 import { useState } from 'react';
 
-export default function FilterComponent() {
-  const [selectedFilters, setSelectedFilters] = useState({
-    searchQuery: '',
-    priceRange: { min: 0, max: 1000 },
-  });
+export default function Home() {
+  const [isSilahTuruOpen, setSilahTuruOpen] = useState(false);
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedFilters((prevState) => ({
-      ...prevState,
-      searchQuery: e.target.value,
-    }));
-  };
-
-  const handlePriceRangeChange = (e: React.ChangeEvent<HTMLInputElement>, type: 'min' | 'max') => {
-    const value = Number(e.target.value);
-    setSelectedFilters((prevState) => ({
-      ...prevState,
-      priceRange: {
-        ...prevState.priceRange,
-        [type]: value,
-      },
-    }));
+  const toggleSilahTuru = () => {
+    setSilahTuruOpen((prevState) => !prevState);
   };
 
   return (
@@ -57,32 +40,32 @@ export default function FilterComponent() {
           <div className="collapse navbar-collapse" id="navbarContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link href="#" className="nav-link active text-white">
+                <Link href="/" className="nav-link active text-white">
                   Market
                 </Link>
               </li>
               <li className="nav-item">
-                <Link href="#" className="nav-link text-white">
+                <Link href="/about" className="nav-link text-white">
                   Envanter
                 </Link>
               </li>
               <li className="nav-item">
-                <Link href="/sell" className="nav-link text-white">
+                <Link href="/services" className="nav-link text-white">
                   Eşya Sat
                 </Link>
               </li>
               <li className="nav-item">
-                <Link href="#" className="nav-link text-white">
+                <Link href="/contact" className="nav-link text-white">
                   Eşya Al
                 </Link>
               </li>
               <li className="nav-item">
-                <Link href="#" className="nav-link text-white">
+                <Link href="/blog" className="nav-link text-white">
                   Açık Artırma
                 </Link>
               </li>
               <li className="nav-item">
-                <Link href="#" className="nav-link text-white">
+                <Link href="/faq" className="nav-link text-white">
                   Yardım
                 </Link>
               </li>
@@ -109,61 +92,67 @@ export default function FilterComponent() {
         </div>
       </nav>
 
-
-      {/* Filtreleme Kutusu */}
+      {/* Filtreleme kutusu */}
       <div className="filter-container">
         <h5>Filtrele</h5>
-
-        {/* Arama Çubuğu */}
-        <form className="d-flex me-2">
-        <div className="input-group">
-          <span className="input-group-text search-icon">
-          <i className="bi bi-search"></i>
-          </span>
-          <input
-          className="form-control search-bar"
-          type="search"
-          placeholder="Arama yap"
-          aria-label="Search"
-          />
+        <div className="dropdown dropdown-horizontal">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownSilahTuru"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            onClick={toggleSilahTuru}
+          >
+            Silah Türü
+          </button>
+          {/* Silah Türü menüsünün içeriği */}
+          {isSilahTuruOpen && (
+            <div className="extra-dropdowns">
+              {["Bıçaklar", "Eldivenler", "Tüfekler", "Tabancalar", "Hafif Makineliler", "Pompalılar", "Makineli Tüfekler", "Çıkartmalar", "Ajanlar", "Diğer"].map((menuName, index) => (
+                <div className="dropdown" key={index}>
+                  <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    id={`dropdownExtra${index}`}
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {menuName}
+                  </button>
+                  <ul className="dropdown-menu dropdown-menu-right" aria-labelledby={`dropdownExtra${index}`}>
+                    <li>
+                      <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="option1" id={`checkbox${index}-1`} />
+                        <label className="form-check-label" htmlFor={`checkbox${index}-1`}>
+                          Karambit
+                        </label>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="option2" id={`checkbox${index}-2`} />
+                        <label className="form-check-label" htmlFor={`checkbox${index}-2`}>
+                          Bayonet M9
+                        </label>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="option3" id={`checkbox${index}-3`} />
+                        <label className="form-check-label" htmlFor={`checkbox${index}-3`}>
+                          Gölge Hançerler
+                        </label>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      </form>
-
-        {/* Fiyat Aralığı */}
-        <div className="price-range mt-3">
-          <label>Fiyat Aralığı</label>
-
-          {/* Fiyat Aralığı Inputları */}
-          <div className="d-flex justify-content-between">
-            <input
-              type="number"
-              className="form-control"
-              min="0"
-              max="10000"
-              value={selectedFilters.priceRange.min}
-              onChange={(e) => handlePriceRangeChange(e, 'min')}
-              style={{ width: '48%' }}
-            />
-            <input
-              type="number"
-              className="form-control"
-              min="0"
-              max="10000"
-              value={selectedFilters.priceRange.max}
-              onChange={(e) => handlePriceRangeChange(e, 'max')}
-              style={{ width: '48%' }}
-            />
-          </div>
-        </div>
-
-        {/* Filtre Uygula */}
-        <button className="btn-apply" type="button">
-        Uygula
-      </button>
       </div>
-
-
-
+      
       {/* Kartlar */}
       <div className="container mt-4">
         <div className="row">
